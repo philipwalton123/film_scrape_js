9a5738736d1ecfd6d51cb8038218b363c8a1e664
Phil Walton <philipwalton123@googlemail.com>
2022-09-21 14:43:32 +0100

    initial commit

--- film_scrape_fe/src/App.js
+++ film_scrape_fe/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import axios from 'axios'
+import { useState } from 'react';
 import './App.css';
+import InputBar from './components/InputBar.js';
+import List from './components/List.js';
 
 function App() {
+
+  const [roles, setRoles] = useState()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">FilmScrape</header>
+      <InputBar setRoles={setRoles}/>
+      <List roles={roles}/>
     </div>
   );
 }
